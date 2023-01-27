15d5d42701df27bca97bd4c242f2e9ad47c5068a
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:59:37 +0100

    Add files via upload

---
+++ Pattern_Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pattern_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regierung p1 = Regierung.GetInstance();
+            p1.name = "Kanzler Scholz";
+
+            Regierung p2 = Regierung.GetInstance();
+            p2.name = "Putsch-Kanzlerin Baerbock";
+
+            Console.WriteLine(p1.name);
+            Console.WriteLine(p2.name);
+        }
+    }
+}
---
+++ Pattern_Singleton/Regierung.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Singleton         //Creational Pattern
+{
+    //Diese Klasse darf nur einmal instanziiert werden
+    //Sollte diese Klasse ein 2. mal instanziiert werden, soll keine neue Instanz gebildet werden, sondern
+    //nur die bereits bestehende Instanz übergeben werden
+    class Regierung
+    {
+        //Felder
+        public string name;
+
+        //statische Felder
+        private static Regierung instance;
+
+        //Konstruktor
+        private Regierung() { }
+
+        //statische Methoden
+        public static Regierung GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Regierung();
+            }
+            return instance;
+        }
+    }
+}
