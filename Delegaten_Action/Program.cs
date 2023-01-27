52c7630224f41d896125ad14ff2bf3ac903435aa
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:56:28 +0100

    Add files via upload

---
+++ Delegaten_Action/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegaten_Action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action: ohne Rückgabewert, bis zu 16 Parameter
+            Action<bool> a1 = Anzeigen;
+            a1(true);
+            a1(false);
+            Console.WriteLine();
+
+            Action a2 = Ausgabe;
+            a2();
+
+        }
+        static void Ausgabe()
+        {
+            Console.WriteLine("Hallo");
+        }
+        static void Anzeigen(bool b)
+        {
+            if (b)
+                Console.WriteLine("Ist true.");
+            else
+                Console.WriteLine("Ist false.");
+        }
+    }
+}
