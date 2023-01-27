ea4f5022f04017ab3c42ce7dac7f0e943889f0a6
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:58:03 +0100

    Add files via upload

---
+++ Delegaten_Predicate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegaten_Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Predicate: immer Rückgabewert bool, genau ein Parameter
+            Predicate<int> p1 = Entscheidung;
+            Console.WriteLine(p1(15));
+
+            Func<int, bool> f1 = Entscheidung;
+            Console.WriteLine(f1(15));
+
+        }
+        static bool Entscheidung(int a)
+        {
+            if (a > 10)
+                return true;
+            else
+                return false;
+        }
+    }
+}
