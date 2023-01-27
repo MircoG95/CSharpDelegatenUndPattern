1a74a29d59ea4a3ece3f96a130ccad8a47b599d3
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:57:25 +0100

    Add files via upload

---
+++ Delegaten_Func/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegaten_Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func: einen Rückgabewert und von 0 bis 16 Parameter
+            //Minimum: Rückgabewert ohne Parameter
+            Func<int, int, int> f = Subtrahieren;
+
+            Console.WriteLine(f(2,3));
+            Console.WriteLine(f.Invoke(10, 5));
+
+            Func<int> f2 = Zufall;
+
+            Console.WriteLine(f2());
+
+        }
+        static int Zufall()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 11);
+        }
+        static int Subtrahieren(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
