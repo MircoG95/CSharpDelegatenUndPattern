31b39bec8b962356ed15d9013469e1160f378842
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:56:58 +0100

    Add files via upload

---
+++ Delegaten_AnonymeMethoden/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegaten_AnonymeMethoden
+{
+    public delegate int Rechnen(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rechnen r1 = delegate (int z1, int z2)
+            {
+                return z1 + z2;
+            };
+
+            int zahl = r1(5, 7);
+            Console.WriteLine(zahl);
+
+            zahl = r1(12, 18);
+            Console.WriteLine(zahl);
+
+            r1 = null;
+        }
+        private static int Addieren(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
